4d635837f948e1574de7d0dc5edfbf87dd22dbe5
Jeonggon Kim'git <kjjkjykjk@gmail.com>
2023-07-09 00:20:23 +0900

    number6

---
+++ main2.py
@@ -0,0 +1,58 @@
+import math 
+import random
+
+def lose_life () : #괄호안에 loser 들어감, 6번 담당
+   
+    return
+
+player_dic = {} # {'player': [life, left_life] }
+
+
+#1
+
+#2
+
+#3 
+    
+#4
+
+#5
+
+#6
+
+#7
+
+#Game 1 정환희
+
+#Game 2 이정한
+
+#Game 3 이현지
+
+#Game 4 곽민경
+
+#Game 5 김정곤
+
+def main_game ():
+    pass
+    #1
+
+    #2
+
+    #3
+
+    #4
+
+    #while (True) :
+        #5
+        
+        #game 1,2,3,4,5
+        
+        #6
+
+    #7
+
+main_game()
+
+
+
+
