84e6248b17a2b937e99a729d377a0dc7d548ef1c
minkyung_kwak <ellen10min@gmail.com>
2023-07-09 22:30:28 +1000

    Modify: 주석 제거

--- game4.py
+++ game4.py
@@ -1,19 +1,23 @@
 import random
 import time
 
+
+players = ['A', 'B', 'C', 'user']
+
 def mouse_game(players):
     phrases = ['쥐', '를', '잡', '자', '쥐를', '잡자', '쥐를 잡자', '찍찍찍', '몇 마리?']
+    active_choice = ['잡았다', '놓쳤다', '풀었다']
     loser = ''
 
-    # for i in range(0, len(phrases)):
-    #     if i < len(players): 
-    #         phrase = phrases[i]
-    #         print(players[i] + ':', phrase)
-    #         time.sleep(0.5)
-    #     else:
-    #         phrase = phrases[i]
-    #         print(players[i % len(players)] + ':', phrase)
-    #         time.sleep(0.5)
+    for i in range(0, len(phrases)):
+        if i < len(players): 
+            phrase = phrases[i]
+            print(players[i] + ':', phrase)
+            time.sleep(0.5)
+        else:
+            phrase = phrases[i]
+            print(players[i % len(players)] + ':', phrase)
+            time.sleep(0.5)
     while True:
             caught_mice = 0
             play_turn = players[0]
@@ -30,54 +34,53 @@ def mouse_game(players):
     
     caught_mice = 0
     play_turn = players[0]
-
     if caught_mice < num_mice:
-        play_turn = players[0]
         while True:
             if play_turn != 'user':
                 phrase = random.choice(['잡았다', '놓쳤다', '풀었다'])
                 if phrase == '잡았다':
                     caught_mice += 1
-                    print(play_turn + ':' + phrase, caught_mice)
+                    print(play_turn + ':' + phrase)
                     time.sleep(0.5)
                 if phrase == '풀었다':
                     if caught_mice > 0:
                         caught_mice -= 1
-                        print(play_turn + ':' + phrase, caught_mice)
+                        print(play_turn + ':' + phrase)
                         time.sleep(0.5)
                 if phrase == '놓쳤다':
-                    print(play_turn + ':' + phrase, caught_mice)
+                    print(play_turn + ':' + phrase)
                     time.sleep(0.5)
             else:
                 user_phrase = input("행동을 선택해주세요: ['잡았다', '놓쳤다', '풀었다']: ")
                 if user_phrase == '잡았다':
-                    print(play_turn + ':' + user_phrase, caught_mice)
+                    print(play_turn + ':' + user_phrase)
                     time.sleep(0.5)
                     caught_mice += 1
 
                 if user_phrase == "놓쳤다":
-                    print(play_turn + ':' + phrase, caught_mice)
+                    print(play_turn + ':' + user_phrase)
                     time.sleep(0.5)
 
                 if user_phrase == '풀었다':
                     if caught_mice > 0:
                         caught_mice -= 1
-                        print(play_turn + ':' + phrase, caught_mice)
+                        print(play_turn + ':' + user_phrase)
                         time.sleep(0.5)
                     else:
-                        print(play_turn + ':' + phrase, caught_mice)
+                        print(play_turn + ':' + user_phrase)
                         print("쥐는 음수가 될 수 없습니다. 찍찍 > ' A ' <")
-                        loser = "user"
-                        print(f"{loser} 의 패배!")
+                        user = loser
+                        print(user, "의 패배!")
                         time.sleep(0.5)
                         return loser
                 
-                else:
-                    print("박자는 생명!! 아 박자는 생명!!!")
+                if user_phrase not in active_choice:
+                    print("찍찍찍찍찍!!!> ' A ' <!!!:")
                     loser="user"
                     print(f"{loser} 의 패배!")
                     time.sleep(0.5)
                     return loser
+                
                 
             play_turn = players[(players.index(play_turn) + 1) % len(players)]
 
@@ -90,7 +93,7 @@ def mouse_game(players):
                 if result == '야옹' and end_time - start_time <= 3:
                     players.remove("user")
                     loser = random.choice(players)
-                    print(f"{loser} 의 패배!")
+                    print(loser, "의 패배!")
                     return loser
 
                 else:
@@ -105,7 +108,6 @@ def mouse_game(players):
         time.sleep(0.5)
         return loser
 
-players = ['A', 'B', 'C', 'user']
 loser = mouse_game(players)
 print("쥐를 잡기 게임에서 패배자는!")
 print(loser)
